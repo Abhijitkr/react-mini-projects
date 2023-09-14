8ac8fa5e0281207dacc0ddaaef2c66015163f23b
Abhijit <abhijitkr600@gmail.com>
2023-09-14 12:03:52 +0530

    Adds Context Setup and Login Component

--- mini-context-api/src/App.jsx
+++ mini-context-api/src/App.jsx
@@ -1,13 +1,16 @@
-
 import './App.css'
+import { Login } from './components/Login'
+import { UserContextProvider } from './context/UserContextProvider'
+
 
 function App() {
 
 
   return (
-    <>
+    <UserContextProvider>
       <h1>React Concept of Context API</h1>
-    </>
+      <Login />
+    </UserContextProvider>
   )
 }
 
