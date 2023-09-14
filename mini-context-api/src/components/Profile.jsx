a25885bbe59c686f34a23e8440a3ffd12eb89929
Abhijit <abhijitkr600@gmail.com>
2023-09-14 12:42:15 +0530

    Adds the initial project setup of theme-changer project

--- mini-context-api/src/components/Profile.jsx
+++ mini-context-api/src/components/Profile.jsx
@@ -3,7 +3,6 @@ import { UserContext } from "../context/UserContext"
 
 export function Profile() {
     const { user } = useContext(UserContext);
-    console.log(user)
     if (!user) return <span>Invalild Username or Password!</span>
     else return <span>Welcome {user.username}!</span>
 }
