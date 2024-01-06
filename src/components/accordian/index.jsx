d09b8c2d99ad3d8725896e879a906d8db482a0a5
AbhijitKr <abhijitkr600@gmail.com>
2024-01-06 18:11:46 +0530

    Adds multi selection feature in accordion project also allows dynamic class and button text

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -4,13 +4,35 @@ import "./style.css";
 
 export default function Accordion() {
   const [selected, setSelected] = useState(null);
+  const [isMulti, setIsMulti] = useState(false);
+  const [multiSelection, setMultiSelection] = useState([]);
+  const [btnClass, setBtnClass] = useState("not-active");
 
-  const handleClick = (getDataId) => {
-    setSelected(getDataId);
+  const handleSingle = (getDataId) => {
+    setSelected(getDataId === selected ? null : getDataId);
+  };
+
+  const onBtnClick = () => {
+    setIsMulti(!isMulti);
+    if (!isMulti) setBtnClass("active");
+    else setBtnClass("not-active");
+  };
+
+  const handleMultiple = (getDataId) => {
+    var cpyMulti = [...multiSelection];
+    var findIndex = cpyMulti.indexOf(getDataId);
+    if (findIndex === -1) cpyMulti.push(getDataId);
+    else cpyMulti.splice(findIndex, 1);
+    setMultiSelection(cpyMulti);
   };
 
   return (
     <div className="wrapper">
+      <div className="btn-grp">
+        <button onClick={onBtnClick} className={btnClass}>
+          {!isMulti ? "Enable" : "Disable"} Multi Selection
+        </button>
+      </div>
       {data &&
         data.length > 0 &&
         data.map((dataItem) => (
@@ -18,15 +40,21 @@ export default function Accordion() {
             <div
               className="title"
               onClick={() =>
-                handleClick(dataItem.id === selected ? null : dataItem.id)
+                isMulti
+                  ? handleMultiple(dataItem.id)
+                  : handleSingle(dataItem.id)
               }
             >
               <div>{dataItem.question}</div>
               <span>+</span>
             </div>
-            {selected === dataItem.id && (
-              <div className="answer">{dataItem.answer}</div>
-            )}
+            {isMulti
+              ? multiSelection.indexOf(dataItem.id) !== -1 && (
+                  <div className="answer">{dataItem.answer}</div>
+                )
+              : selected === dataItem.id && (
+                  <div className="answer">{dataItem.answer}</div>
+                )}
           </div>
         ))}
     </div>
